4b8ab30baa16d4341c6da322be20c11df1ba6e0b
Walid Shaari <walid.shaari@gmail.com>
2023-08-27 15:00:05 +0000

    restructure

---
+++ s3-bucket.tf
@@ -0,0 +1,12 @@
+module "s3_bucket" {
+
+  source = "terraform-aws-modules/s3-bucket/aws"
+  bucket_prefix = ${prefix}-0321:
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}

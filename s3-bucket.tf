9ee27a31d612ed3a73d9162658d67004e3327be2
Walid Shaari CNJ <walid.shaari@linux.com>
2023-08-27 18:14:28 +0300

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source  = "app.terraform.io/walid-training/s3-bucket/aws"
   version = "2.8.0"
-  bucket_prefix = "${prefix}-0321"
+  bucket_prefix = "${prefix}"
   bucket = "my-s3-bucket"
   acl    = "private"
 
